91d3d4fca9fe11cdf3dc96c9f5a5562819bbf217
Jefferson Rodrigues <jeffersonrodrigues5030@gmail.com>
2020-11-09 19:04:19 -0300

    iniciando querys no bd

--- src/app/controllers/instructors.js
+++ src/app/controllers/instructors.js
@@ -19,9 +19,21 @@ module.exports = {
             }
         }
      
-        let {avatar_url, birth, name, gender, services} = req.body
+        //let {avatar_url, birth, name, gender, services} = req.body
         
-            return
+        const query = `
+            INSERT INTO instructors (
+                name,
+                avatar_url,
+                gender,
+                services,
+                birth,
+                created_at
+            ) VALUES ($1, $2, $3, $4, $5, $6)
+            RETURNING id
+        `
+
+        return
 
     },
 
