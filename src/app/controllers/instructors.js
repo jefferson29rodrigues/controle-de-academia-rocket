e7fbc0d04b100f4a273acf6512da5858f008dd89
Jefferson Rodrigues <jeffersonrodrigues5030@gmail.com>
2020-11-11 16:32:49 -0300

    update concluido

--- src/app/controllers/instructors.js
+++ src/app/controllers/instructors.js
@@ -45,7 +45,13 @@ module.exports = {
     },
 
     edit(req, res){
-        return
+        Instructor.find(req.params.id, function(instructor) {
+            if (!instructor) return res.send("Instructor not found!")
+
+            instructor.birth = date(instructor.birth).iso
+
+            return res.render("instructors/edit", { instructor })
+        })
     },
 
     put(req, res){
@@ -58,7 +64,9 @@ module.exports = {
             }
         }
 
-        return
+        Instructor.update(req.body, function() {
+            return res.redirect(`/instructors/${req.body.id}`)
+        })
     },
 
     delete(req, res){
