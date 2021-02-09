e5a1856f2e71f379d28c63f956b58f95a76a4937
Jefferson Rodrigues <jeffersonrodrigues5030@gmail.com>
2021-02-09 11:11:43 -0300

    filter por nome ok

--- src/app/controllers/instructors.js
+++ src/app/controllers/instructors.js
@@ -5,10 +5,20 @@ const Instructor = require('../models/Instructor')
 module.exports = {
     index(req, res){
 
+        const { filter } = req.query
+
+        if ( filter ) {
+            Instructor.findBy(filter, function(instructors) {
+                return res.render('instructors/index', { instructors })
+            })
+        } else {
+            Instructor.all(function(instructors) {
+                return res.render('instructors/index', { instructors })
+            })        
+        }
+        
         console.log(req.query)
-        Instructor.all(function(instructors) {
-            return res.render('instructors/index', { instructors })
-        })
+        
     },
 
     create(req, res){
