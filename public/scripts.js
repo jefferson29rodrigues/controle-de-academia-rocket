c5c2b3980f31d51ebaebc72ce1f8a2f2a3f5c94c
Jefferson Rodrigues <jeffersonrodrigues5030@gmail.com>
2021-02-09 18:19:48 -0300

    mostra primeiro e ultimo numero do array pages

--- public/scripts.js
+++ public/scripts.js
@@ -14,3 +14,21 @@ for (item of menuItems) {
         item.classList.add("active")
     }
 }
+
+// Paginação
+// totalPages = 20
+// selectedPage = 15
+// [1, ..., 13, 14, 15, 16, 17, ..., 20]
+
+let totalPages = 20,
+    selectedPage = 15,
+    pages = []
+
+for(let currentPage = 1; currentPage <= totalPages; currentPage++) {
+
+    if (currentPage == 1 || currentPage == totalPages) {
+        pages.push(currentPage)
+    }
+}
+
+console.log(pages)
