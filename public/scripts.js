d90b59c5129f17abae0fea8df8b3e68421b773f6
Jefferson Rodrigues <jeffersonrodrigues5030@gmail.com>
2021-02-10 12:47:32 -0300

    terminado algoritmo de paginacao

--- public/scripts.js
+++ public/scripts.js
@@ -22,12 +22,28 @@ for (item of menuItems) {
 
 let totalPages = 20,
     selectedPage = 15,
-    pages = []
+    pages = [],
+    oldPage
 
 for(let currentPage = 1; currentPage <= totalPages; currentPage++) {
 
-    if (currentPage == 1 || currentPage == totalPages) {
+    const firstAndLastPage = currentPage == 1 || currentPage == totalPages
+    const pagesAfterSelectedPage = currentPage <= selectedPage + 2
+    const pagesBeforeSelectedPage = currentPage >= selectedPage - 2
+
+    if (firstAndLastPage || pagesBeforeSelectedPage && pagesAfterSelectedPage) {
+
+        if (oldPage && currentPage - oldPage > 2) {
+            pages.push('...')
+        }
+
+        if (oldPage && currentPage - oldPage == 2) {
+            pages.push(oldPage + 1)
+        }
+
         pages.push(currentPage)
+
+        oldPage = currentPage
     }
 }
 
